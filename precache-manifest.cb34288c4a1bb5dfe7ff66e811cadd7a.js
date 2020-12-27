1d8362a68591f5e868e5c2cdccc3afbbf7f768d2
sidewaysdesign <richard@sidewaysdesign.com>
2020-12-27 12:19:14 -0500

    Updates

---
+++ precache-manifest.cb34288c4a1bb5dfe7ff66e811cadd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b173040b024c199cf3638d4f9ba78738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f291757fe9d064bf28fd",
+    "url": "/static/css/main.b875b494.chunk.css"
+  },
+  {
+    "revision": "baa949be91e183fbbe7a",
+    "url": "/static/js/2.175d4dec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.175d4dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f291757fe9d064bf28fd",
+    "url": "/static/js/main.e475eabb.chunk.js"
+  },
+  {
+    "revision": "b8fd8fb502bef6b63202",
+    "url": "/static/js/runtime-main.21e8196f.js"
+  }
+]);
