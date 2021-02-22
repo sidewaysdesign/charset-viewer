3fe9af50501aec3a10ba83be7a0270456c479fab
sidewaysdesign <richard@sidewaysdesign.com>
2021-02-22 12:26:32 -0500

    republish

---
+++ build/precache-manifest.7896f4568a0956bb7dd165ab883b538c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23a92accc3253b23b5cbaa123b217ef",
+    "url": "/charset-viewer/index.html"
+  },
+  {
+    "revision": "5579e548e9132d38ff95",
+    "url": "/charset-viewer/static/css/main.b875b494.chunk.css"
+  },
+  {
+    "revision": "5b4b15318ad6fd41c05f",
+    "url": "/charset-viewer/static/js/2.fe63b893.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/charset-viewer/static/js/2.fe63b893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5579e548e9132d38ff95",
+    "url": "/charset-viewer/static/js/main.8643c598.chunk.js"
+  },
+  {
+    "revision": "87aba12d1ded4798b634",
+    "url": "/charset-viewer/static/js/runtime-main.dddcbb26.js"
+  }
+]);
