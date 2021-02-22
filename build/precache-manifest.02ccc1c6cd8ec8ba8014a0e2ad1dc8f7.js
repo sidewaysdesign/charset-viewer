94d05064260ac1b19daa875abe527ec3978b1c97
sidewaysdesign <richard@sidewaysdesign.com>
2021-02-22 11:44:09 -0500

    remove gitignore files

---
+++ build/precache-manifest.02ccc1c6cd8ec8ba8014a0e2ad1dc8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a1b7f27b7667cc5d36ae885fc8cc7b",
+    "url": "/charset-viewer/index.html"
+  },
+  {
+    "revision": "9fa0293a26a5ba3aa49c",
+    "url": "/charset-viewer/static/css/main.b875b494.chunk.css"
+  },
+  {
+    "revision": "baa949be91e183fbbe7a",
+    "url": "/charset-viewer/static/js/2.175d4dec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/charset-viewer/static/js/2.175d4dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa0293a26a5ba3aa49c",
+    "url": "/charset-viewer/static/js/main.fea91d9b.chunk.js"
+  },
+  {
+    "revision": "87aba12d1ded4798b634",
+    "url": "/charset-viewer/static/js/runtime-main.dddcbb26.js"
+  }
+]);
